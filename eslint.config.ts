45d090f1f3afa291db945f5947596b47ac0b57ea
JIAXInT <1131918725@qq.com>
2025-05-13 17:44:38 +0800

    style: 更新代码格式化配置和依赖
    更新了 `.prettierrc.json` 和 `.editorconfig` 文件以统一代码格式化规则，并添加了 `eslint-config-prettier` 和 `eslint-plugin-prettier` 依赖以确保 ESLint 和 Prettier 的兼容性。同时调整了部分代码的格式以提高可读性。

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,8 @@
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs
+} from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
@@ -11,12 +14,12 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{ts,mts,tsx,vue}']
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  skipFormatting,
+  skipFormatting
 )
