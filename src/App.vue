b3613d1bd02a2004a665aa0f98363962b5b34eb9
JIAXInT <1131918725@qq.com>
2025-05-15 10:44:17 +0800

    feat: 添加 Element Plus 组件库及自动导入配置
    引入 Element Plus 组件库，并配置 unplugin-auto-import 和 unplugin-vue-components 实现自动导入功能，简化组件和 API 的使用。

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
     <router-link to="/main">主页面</router-link>
     <router-view></router-view>
   </div>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 import { viteMockServe } from 'vite-plugin-mock'
@@ -15,6 +18,12 @@ export default defineConfig({
       mockPath: './src/mock',
       enable: true,
       logger: true
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
     })
   ],
   resolve: {
